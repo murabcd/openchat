0940664d6e5078a9a70361ce96212eac8fd321b0
muradpm <abdulkadyr0v@yandex.ru>
2025-02-09 12:32:27 +0300

    add chat with image

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,63 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  const [files, setFiles] = useState<FileList | undefined>(undefined);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m) => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === "user" ? "User: " : "AI: "}
           {m.content}
+          <div>
+            {m?.experimental_attachments
+              ?.filter((attachment) => attachment?.contentType?.startsWith("image/"))
+              .map((attachment, index) => (
+                <Image
+                  key={`${m.id}-${index}`}
+                  src={attachment.url}
+                  width={500}
+                  height={500}
+                  alt={attachment.name ?? `attachment-${index}`}
+                />
+              ))}
+          </div>
         </div>
       ))}
 
-      <form onSubmit={handleSubmit}>
+      <form
+        className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl space-y-2"
+        onSubmit={(event) => {
+          handleSubmit(event, {
+            experimental_attachments: files,
+          });
+
+          setFiles(undefined);
+
+          if (fileInputRef.current) {
+            fileInputRef.current.value = "";
+          }
+        }}
+      >
         <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          type="file"
+          onChange={(event) => {
+            if (event.target.files) {
+              setFiles(event.target.files);
+            }
+          }}
+          multiple
+          ref={fileInputRef}
+        />
+        <input
+          className="dark:bg-zinc-900 w-full p-2 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
           value={input}
           placeholder="Say something..."
           onChange={handleInputChange}
