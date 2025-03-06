814b9e3750744eaff074347c1e17e8f4a113a067
muradpm <abdulkadyr0v@yandex.ru>
2025-03-06 12:51:05 +0300

    add icons to suggestions

--- components/suggested-actions.tsx
+++ components/suggested-actions.tsx
@@ -3,6 +3,7 @@
 import { memo } from "react";
 
 import { ChatRequestOptions, CreateMessage, Message } from "ai";
+import { Code, Lightbulb, ScanText, ThermometerSun } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
@@ -19,23 +20,27 @@ function PureSuggestedActions({ chatId, append }: SuggestedActionsProps) {
   const suggestedActions = [
     {
       title: "Code",
-      label: "Djikstra's algorithm",
+      label: "demonstrating Dijkstra's algorithm",
       action: "Write code that demonstrates Djikstra's algorithm.",
+      icon: <Code className="w-4 h-4 mr-2" />,
     },
     {
       title: "Explain",
-      label: "Relativity theory",
+      label: "the theory of relativity",
       action: "Explain the theory of relativity in simple terms.",
+      icon: <Lightbulb className="w-4 h-4 mr-2" />,
     },
     {
       title: "Summarize",
-      label: "Hamlet plot",
+      label: "the plot of Hamlet",
       action: "Summarize the plot of Hamlet briefly.",
+      icon: <ScanText className="w-4 h-4 mr-2" />,
     },
     {
       title: "Weather",
-      label: "London today",
+      label: "in London today",
       action: "What is the current weather in London?",
+      icon: <ThermometerSun className="w-4 h-4 mr-2" />,
     },
   ];
 
@@ -62,7 +67,10 @@ function PureSuggestedActions({ chatId, append }: SuggestedActionsProps) {
             }}
             className="text-left border rounded-xl px-4 py-3.5 text-sm flex-1 gap-1 sm:flex-col w-full h-auto justify-start items-start"
           >
-            <span className="font-medium">{suggestedAction.title}</span>
+            <div className="flex items-center">
+              {suggestedAction.icon}
+              <span className="font-medium">{suggestedAction.title}</span>
+            </div>
             <span className="text-muted-foreground">{suggestedAction.label}</span>
           </Button>
         </motion.div>
