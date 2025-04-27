d913a21facfd8b4a18f804b17266f9d4053cf1d1
muradpm <abdulkadyr0v@yandex.ru>
2025-04-27 13:23:23 +0300

    update empty state message

--- components/overview.tsx
+++ components/overview.tsx
@@ -5,35 +5,38 @@ import { motion } from "framer-motion";
 
 export const Overview = () => {
   return (
-    <motion.div
+    <div
       key="overview"
-      className="max-w-3xl mx-auto md:mt-20"
-      initial={{ opacity: 0, scale: 0.98 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.98 }}
-      transition={{ delay: 0.5 }}
+      className="max-w-3xl mx-auto md:mt-20 px-8 size-full flex flex-col justify-center"
     >
-      <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p className="flex flex-row justify-center gap-4 items-center">
-          <Image
-            src="/logo-dark.svg"
-            alt="Logo"
-            width={50}
-            height={50}
-            className="dark:hidden"
-          />
-          <Image
-            src="/logo.svg"
-            alt="Logo"
-            width={50}
-            height={50}
-            className="hidden dark:block"
-          />
-        </p>
-        <h1 className="text-4xl font-semibold">What can I help with?</h1>
-        <p className="font-medium text-muted-foreground">
-          Type questions, request code examples, or ask for explanations on any topic.
-          Contribute to project, and show your support on{" "}
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.5 }}
+        className="text-2xl font-semibold"
+      >
+        Hello there!
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.6 }}
+        className="text-2xl text-zinc-500"
+      >
+        How can I help you today?
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.7 }}
+        className="mt-4"
+      >
+        <p className="font-medium text-sm text-muted-foreground">
+          Type questions, request code examples, or ask for explanations on any topic. If
+          you like the project, contribute or star it on{" "}
           <Link
             className="underline underline-offset-4"
             href="https://github.com/muradpm/openchat"
@@ -41,8 +44,9 @@ export const Overview = () => {
           >
             GitHub
           </Link>
+          .
         </p>
-      </div>
-    </motion.div>
+      </motion.div>
+    </div>
   );
 };
