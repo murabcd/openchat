0063ea069f703284e84c4c05aab7baf74eca96cb
muradpm <abdulkadyr0v@yandex.ru>
2025-05-10 18:31:16 +0300

    fix env.mjs after merge

--- env.mjs
+++ env.mjs
@@ -8,8 +8,6 @@ export const env = createEnv({
    */
   server: {
     CONVEX_DEPLOYMENT: z.string().min(1),
-    NEXT_PUBLIC_CONVEX_URL: z.string().min(1).url(),
-    NEXT_PUBLIC_SITE_URL: z.string().min(1).url(),
     OPENAI_API_KEY: z.string().min(1),
     AUTH_GOOGLE_ID: z.string().min(1),
     AUTH_GOOGLE_SECRET: z.string().min(1),
@@ -33,6 +31,7 @@ export const env = createEnv({
     CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
